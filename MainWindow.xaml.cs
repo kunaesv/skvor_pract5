f5b22e9cadb6e116628ed5adf8dd151f664ebcd4
Kirill <skvorkink@gmail.com>
2023-05-26 12:38:55 +0300

    Практика 5 задание 1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace skvor_pract5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int k = -1;
+                int n = Convert.ToInt32(ListBoxData.Items[0]);
+                int kol = 0;
+                int g = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    int m = Convert.ToInt32(ListBoxData.Items[i]);
+                    if (m % 10 == 2)
+                    {
+                        if (m % 3 == 0)
+                        {
+                            kol = g;
+                            g++;
+                            k = 0;
+                        }
+                    }
+                }
+                if (k != -1)
+                {
+                    TextBlockAnswer.Text = $"Ответ:\n{kol}";
+                }
+                else
+                {
+                    TextBlockAnswer.Text = $"Ответ:\n Чисел оканчивающихся на 2 и кратные 3 нет";
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if ((String.IsNullOrEmpty(TbNumber.Text)))
+            {
+                return;
+            }
+            try
+            {
+                int xa = Convert.ToInt32(TbNumber.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введены не корректные данные");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            ListBoxData.Items.Add(TbNumber.Text);
+        }
+    }
+}
